c92fbf47b0b184f99624780f098e975c6812a455
chelpuck0 <chelpuck0@gmail.com>
2022-10-09 01:08:55 +0300

    composer

---
+++ local/php_interface/init.php
@@ -0,0 +1,6 @@
+<?
+use Bitrix\Main\Application;
+
+// Подключаем автолоад классов с composer
+require_once(Application::getDocumentRoot() . '/local/vendor/autoload.php');
+?>
---
+++ local/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'beab7656814a5e2df640fbeb59aaacc35fdc7448',
+        'name' => 'godra/api',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'godra/api' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'beab7656814a5e2df640fbeb59aaacc35fdc7448',
+            'dev_requirement' => false,
+        ),
+    ),
+);
