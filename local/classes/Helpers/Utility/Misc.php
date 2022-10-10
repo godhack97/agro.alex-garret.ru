31870f237b9a29e6b636047eccf37cdea6462db5
chelpuck0 <chelpuck0@gmail.com>
2022-10-10 14:53:25 +0300

    заголовки

--- local/classes/Helpers/Utility/Misc.php
+++ local/classes/Helpers/Utility/Misc.php
@@ -36,7 +36,13 @@ class Misc {
         }
 
         if($type == 'mandatory')
+        {
             header("Access-Control-Allow-Origin: *");
+            header("Access-Control-Max-Age: 3600");
+            header("Access-Control-Allow-Headers: access");
+            header("Access-Control-Allow-Methods: POST");
+            header("Access-Control-Allow-Credentials: true");
+        }
     }
 
     /**
